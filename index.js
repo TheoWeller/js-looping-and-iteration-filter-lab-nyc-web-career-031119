87adbb2304ac41d1f1990eeed82e529aff268b15
Theo Weller <weller.theodor@gmail.com>
2019-04-22 22:38:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file
 function findMatching(collection, attribute) {
-  newArray = collection.filter(name => name === attribute);
+  newArray = collection.filter(name => name.toLowerCase() === attribute.toLowerCase());
   return newArray;
 };
