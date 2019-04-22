7ffd6f7e00043881a7d6f48adefd060f3d77ad6c
Theo Weller <weller.theodor@gmail.com>
2019-04-22 22:59:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function findMatching(collection, attribute) {
   newArray = collection.filter(name => name.toLowerCase() === attribute.toLowerCase());
   return newArray;
 };
+
+function fuzzyMatch(collection, attribute) {
+  let match = attribute.slice(0, 2);
+  collection.filter(name => name.slice(0, 2) == match);
+}
