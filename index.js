1ed66968430ec6acf4545dfeba454d91af33e788
Theo Weller <weller.theodor@gmail.com>
2019-04-22 22:34:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file
 function findMatching(collection, attribute) {
-  collection.filter(function (name) {return name ? name === attribute});
+  collection.filter(name => name === attribute);
   return newArray;
 };
